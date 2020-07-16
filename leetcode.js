def60186eff29bd9feb247ffcaeb7a3529b6205c
Tiffany <chin.tiffany108@gmail.com>
2020-07-16 12:03:30 -0400

    7/16

--- leetcode.js
+++ leetcode.js
@@ -15,9 +15,46 @@
 //------------------------------------------------------------------------------
 
 
+
+
 //------------------------------------------------------------------------------
 
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string.Ignore capitalization when determining if a character is a duplicate.
 
+//   Examples
+// "din"      => "((("
+// "recede"   => "()()()"
+// "Success"  => ")())())"
+// "(( @"     => "))((" 
+
+function duplicateEncode(word) {
+  word = word.toLowerCase();
+  let count = {};
+  let newStr = "";
+
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+    if (!count[char]) {
+      count[char] = 1;
+    } else {
+      count[char] += 1;
+    }
+  }
+
+  for (let j = 0; j < word.length; j++) {
+    let char = word[j];
+    if (count[char] === 1) {
+      newStr += "(";
+    } else {
+      newStr += ")";
+    }
+  }
+
+  return newStr;
+}
+
+// let word = "Success";
+// duplicateEncode(word);
 
 //------------------------------------------------------------------------------
 
