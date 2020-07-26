eba1dddd4cc8281df8ac41340d09652c02cd4c2d
Tiffany <chin.tiffany108@gmail.com>
2020-07-26 00:41:22 -0400

    7/25

--- codewars.js
+++ codewars.js
@@ -4,14 +4,6 @@
 
 
 
-//------------------------------------------------------------------------------
-
-
-
-
-//------------------------------------------------------------------------------
-
-
 
 //------------------------------------------------------------------------------
 
@@ -22,14 +14,221 @@
 
 
 
+//------------------------------------------------------------------------------
+
+// Are they the "same"?
+
+function comp(array1, array2) {
+  let newArr1 = array1.map(num => { return num * num});
+
+  // debugger;
+  for (let i = 0; i < array2.length; i++) {
+    let num = array2[i];
+    if (newArr1.includes(num)) {
+      let idx = newArr1.indexOf(num);
+      newArr1.splice(idx, 1);
+      // debugger
+    } else {
+      // debugger;
+      return false;
+    }
+  }
+
+  // debugger;
+
+  return newArr1.length === 0;
+}
+
+let a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+let a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19];
+comp(a1, a2);   // true
 
 //------------------------------------------------------------------------------
 
+// Consecutive strings
 
-// Sum of Pairs
+function longestConsec(strarr, k) {
+  if (k <= 0 || strarr.length === 0 || k > strarr.length) return "";
 
-var sum_pairs = function (ints, s) {
-  //your code here
+  let longest;
+
+  for (let i = 0; i < strarr.length; i++) {
+    let sub = strarr.slice(i, i + k).join("");
+    if (!longest || longest.length < sub.length) {
+      longest = sub;
+    } 
+  }
+
+  return longest;
+}
+
+longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
+longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1)
+longestConsec([], 3)
+longestConsec(["itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2)
+longestConsec(["wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu"], 2)
+longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2)
+longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3)
+longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 15)
+longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 0)
+
+
+
+//------------------------------------------------------------------------------
+
+// Equal Sides Of An Array
+
+function findEvenIndex(arr) {
+  let idx = -1;
+  for (let i = 0; i < arr.length; i++) {
+    let left = arr.slice(0, i);
+    let right = arr.slice(i + 1);
+
+    if (i === 0) {
+      let rightSum = right.reduce((a, b) => a + b);
+      if (rightSum === 0) return i;
+    } else if (i === arr.length - 1) {
+      let leftSum = left.reduce((a, b) => a + b);
+      if (leftSum === 0) return i;
+    } else {
+      let leftSum = left.reduce((a, b) => a + b);
+      let rightSum = right.reduce((a, b) => a + b);
+      if (leftSum === rightSum) return i;
+    }
+  }
+
+  return idx;
+}
+
+
+//------------------------------------------------------------------------------
+
+// Counting Duplicates
+
+function duplicateCount(text) {
+  if (text.length === 0) return 0;
+
+  text = text.toLowerCase();
+  let counts = {};
+
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i];
+    if (!counts[char]) counts[char] = 0;
+    counts[char] += 1;
+  }
+
+  keys = Object.keys(counts).filter(key => counts[key] > 1);
+  return keys.length;
+}
+
+
+
+//------------------------------------------------------------------------------
+
+// // Best travel
+// // ls is an array
+// // t is max of k nums
+
+// function chooseBestSum(t, k, ls) {
+//   let townTrios = [];
+//   for (let i = 0; i < ls.length; i++) {
+//     for (let j = i + 1; j < ls.length; j++) {
+//       for (let h = j + 1; h < ls.length; h++) {
+//         townTrios.push([ls[i], ls[j], ls[k]]);
+//       }
+//     }
+//   }
+
+//   debugger;
+
+//   let sums = [];
+//   for (let i = 0; i < townTrios.length; i++) {
+//     let towns = townTrios[i];
+//     let sum = towns.reduce((a, b) => a + b);
+//     sums.push(sum);
+//   }
+
+//   debugger;
+
+//   let max = Math.max(sums);
+//   let idx = sums.indexOf(max);
+//   return townTrios[idx];
+// }
+
+// let ts = [50, 55, 56, 57, 58];
+// chooseBestSum(163, 3, ts);
+// ts = [50];
+// chooseBestSum(163, 3, ts);
+// ts = [91, 74, 73, 85, 73, 81, 87];
+// chooseBestSum(230, 3, ts);
+
+
+//------------------------------------------------------------------------------
+
+// Scramblies
+
+function scramble(str1, str2) {
+  let count2 = {};
+  for (let i = 0; i < str1.length; i++) {
+    let char = str1[i];
+    if (!count2[char]) count2[char] = 0;
+    count2[char] += 1;
+  }
+
+  let count = {};
+  for (let i = 0; i < str2.length; i++) {
+    let char = str2[i];
+    if (!count[char]) count[char] = 0;
+    count[char] += 1;
+  }
+
+  return Object.keys(count).every(key => count2[key] >= count[key]);
+}
+
+scramble('rkqodlw', 'world');
+scramble('cedewaraaossoqqyt', 'codewars');
+scramble('katas', 'steak');
+scramble('scriptjava', 'javascript');
+scramble('scriptingjava', 'javascript');
+scramble('scriptsjava', 'javascripts');
+scramble('jscripts', 'javascript');
+scramble('aabbcamaomsccdd', 'commas');
+
+
+//------------------------------------------------------------------------------
+
+// Moving Zeros To The End
+
+var moveZeros = function (arr) {
+  let zeroCount = 0;
+  let prevZeroIdx, newArr;
+
+  for (let i = 0; i < arr.length; i++) {
+    let ele = arr[i];
+    if (ele === 0) {
+      if (!prevZeroIdx) {
+        newArr = arr.slice(0, i);
+        prevZeroIdx = i;
+      } else {
+        let sub = arr.slice(prevZeroIdx + 1, i);
+        newArr = newArr.concat(sub);
+        prevZeroIdx = i;
+      }
+
+      zeroCount += 1;
+    } 
+  }
+
+  let lastSub = arr.slice(prevZeroIdx + 1);
+  newArr = newArr.concat(lastSub);
+
+  for (let i = 0; i < zeroCount; i++) {
+    newArr.push(0);
+  }
+
+  if (arr[0] === 0) newArr = newArr.slice(1);
+
+  return newArr;
 }
 
 
@@ -330,7 +529,7 @@ function anagrams(word, words) {
     }
 
     let isAna = keys.every(key => count[key] === count2[key]);
-    debugger;
+    // debugger;
 
     if (isAna && w !== 'crazer') ana.push(w);
   });
