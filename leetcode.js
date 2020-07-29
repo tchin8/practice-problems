86b9305e34c8c1e66eeb0456ef8fc6c6fa7b1029
Tiffany <chin.tiffany108@gmail.com>
2020-07-28 21:21:11 -0400

    7/28

--- leetcode.js
+++ leetcode.js
@@ -22,6 +22,18 @@
 
 //------------------------------------------------------------------------------
 
+// 1528. Shuffle String
+
+var restoreString = function (s, indices) {
+  let arr = new Array(s.length);
+  for (let i = 0; i < indices.length; i++) {
+    let idx = indices[i];
+    arr[idx] = s[i];
+  }
+
+  return arr.join("");
+};
+
 
 
 //------------------------------------------------------------------------------
