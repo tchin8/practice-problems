973deee0b2295230fe70d2a3b8812139591013b6
Tiffany <chin.tiffany108@gmail.com>
2020-07-29 15:54:50 -0400

    7/29

--- leetcode.js
+++ leetcode.js
@@ -13,6 +13,18 @@
 
 //------------------------------------------------------------------------------
 
+// 136. Single Number
+
+var singleNumber = function (nums) {
+  let count = {};
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    if (!count[num]) count[num] = 0;
+    count[num] += 1;
+  }
+
+  return Object.keys(count).filter(key => count[key] === 1);
+};
 
 
 //------------------------------------------------------------------------------
