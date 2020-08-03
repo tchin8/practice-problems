39df3f2f441fbe0348089ee3abf16da5c741473b
Tiffany <chin.tiffany108@gmail.com>
2020-08-02 20:08:57 -0400

    8/2

--- leetcode.js
+++ leetcode.js
@@ -43,7 +43,18 @@ var permute = function (nums) {
 
 //------------------------------------------------------------------------------
 
+// 169. Majority Element
 
+var majorityElement = function (nums) {
+  let count = {};
+  for (let i = 0; i < nums.length; i++) {
+    let ele = nums[i];
+    if (!count[ele]) count[ele] = 0;
+    count[ele] += 1;
+
+    if (count[ele] > nums.length / 2) return ele;
+  }
+};
 
 
 //------------------------------------------------------------------------------
