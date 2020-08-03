8d2f2d0d9b9de602f4db784ce6ce1a8d1958ab3a
Tiffany <chin.tiffany108@gmail.com>
2020-08-03 19:06:35 -0400

    8/3

--- leetcode.js
+++ leetcode.js
@@ -37,8 +37,20 @@
 // 326. Power of Three
 
 var isPowerOfThree = function (n) {
+  if (n === 0) return false;
+  if (n === 1) return true;
 
+  if (n / 3 !== Math.floor(n / 3)) return false;
+  
+  while (n !== 1) {
+    if (n % 3 !== 0) return false;
+    n /= 3;
+  }
+
+  return n === 1;
 };
+
+console.log(isPowerOfThree(30371328));
 
 
 
