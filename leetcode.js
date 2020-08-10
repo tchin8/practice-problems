979a2a6ccc9703c582ca4b1ea5f86b06dbac5cbd
Tiffany <chin.tiffany108@gmail.com>
2020-08-10 14:19:40 -0400

    8/10

--- leetcode.js
+++ leetcode.js
@@ -22,8 +22,27 @@
 // 448. Find All Numbers Disappeared in an Array
 
 var findDisappearedNumbers = function (nums) {
+  let missing = [];
+  nums = nums.sort((a,b) => a - b);
 
+  for (let i = 1; i < nums.length; i++) {
+    let num = nums[i];
+    let prev = nums[i - 1];
+    if (num - prev > 1) {
+      for (let j = 1; j <= num - prev; j++) {
+        missing.push(prev + i);
+      }
+    }
+  }
+
+  return missing;
 };
+
+// Input:
+// [4, 3, 2, 7, 8, 2, 3, 1]
+
+// Output:
+// [5, 6]
 
 
 
