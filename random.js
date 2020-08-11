7d04a5f759a69d17278c620c927c0fa236836870
Tiffany <chin.tiffany108@gmail.com>
2020-08-11 12:54:41 -0400

    8/11

--- random.js
+++ random.js
@@ -1,5 +1,91 @@
 
 
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+
+
+// Q: Given a 2D board and a word, find if the word exists in the grid.
+
+// The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+// Example:
+
+// board =
+// [
+//   ['A','B','C','E'],
+//   ['S','F','C','S'],
+//   ['A','D','E','E']
+// ]
+
+// Given word = "ABCCED", return true.
+// Given word = "SEE", return true.
+// Given word = "ABCB", return false.
+
+
+function recursiveBoard(board, word) {
+  let firstChar = [];
+
+  for (let i = 0; i < word.length; i++) { // O(n)
+    let char = word[i];
+
+    for (let row = 0; row < board.length; row++) {  // O(n)
+      for (let col = 0; col < board[0].length; col++) {    // O(n)
+        let cell = board[row][col];
+
+        if (cell === char) {
+          firstChar.push([row, col]);
+        }
+      }
+    }
+  }
+
+  if (firstChar.length > 0) {
+    for (let i = 0; i < array.length; i++) { // O(n)
+      let pos = array[i];
+      let used = [pos];
+      return rec2Board(board, word.slice(1), pos, used);
+
+      // horizontal : board[0][1], board[0][-1]
+      // vertocal : board[1][0], board[-1][0]
+    }
+  } else {
+    return false;
+  }
+}
+
+function rec2Board(board, word, pos, used) {
+  if (word.length === 0) return true;
+
+  used = used || [];
+  let [x, y] = pos;
+  let neighbors = [[x, y + 1], [x, y - 1], [x - 1, y], [x + 1, y]];
+  let char = word[i];
+
+  for (let i = 0; i < neighbors; i++) { // O(n)
+    let [x, y] = neighbors[i]; // O(1)
+    let cell = board[x][y];
+    if (cell && cell === char && !used.includes([x, y])) {
+      used.push([x, y]);
+      return rec2Board(board, word.slice(1), [x, y], used);
+    }
+  }
+
+  return word.length === 0;
+}
 
 
 //------------------------------------------------------------------------------
