49edbb28b408545534039e38457019674a05c984
Tiffany <chin.tiffany108@gmail.com>
2020-08-12 21:50:27 -0400

    8/12

--- ds_a/w16d1/sorts.js
+++ ds_a/w16d1/sorts.js
@@ -1,27 +1,44 @@
 // Merge Sort Notes
 
-function merge(array1, array2) {
+// function merge(array1, array2) {
+//   let merged = [];
+
+//   // keep running while either array still contains elements
+//   while (array1.length || array2.length) {
+//     // if array1 is nonempty, take its the first element as ele1
+//     // otherwise array1 is empty, so take Infinity as ele1
+//     let ele1 = array1.length ? array1[0] : Infinity;
+
+//     // do the same for array2, ele2
+//     let ele2 = array2.length ? array2[0] : Infinity;
+
+//     let next;
+//     // remove the smaller of the eles from it's array
+//     if (ele1 < ele2) {
+//       next = array1.shift();
+//     } else {
+//       next = array2.shift();
+//     }
+
+//     // and add that ele to the new array
+//     merged.push(next);
+//   }
+
+//   return merged;
+// }
+
+function merge(arr1, arr2) {
   let merged = [];
 
-  // keep running while either array still contains elements
-  while (array1.length || array2.length) {
-    // if array1 is nonempty, take its the first element as ele1
-    // otherwise array1 is empty, so take Infinity as ele1
-    let ele1 = array1.length ? array1[0] : Infinity;
+  while (arr1.length || arr2.length) {
+    let num1 = arr1.length ? arr1[0] : Infinity;
+    let num2 = arr2.length ? arr2[0] : Infinity;
 
-    // do the same for array2, ele2
-    let ele2 = array2.length ? array2[0] : Infinity;
-
-    let next;
-    // remove the smaller of the eles from it's array
-    if (ele1 < ele2) {
-      next = array1.shift();
+    if (num1 > num2) {
+      merged.push(arr2.shift());
     } else {
-      next = array2.shift();
+      merged.push(arr1.shift());
     }
-
-    // and add that ele to the new array
-    merged.push(next);
   }
 
   return merged;
