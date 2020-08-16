147c687b0198f87f4db801c805efb84a2259c02d
Tiffany <chin.tiffany108@gmail.com>
2020-08-15 20:04:19 -0400

    8/15

--- random.js
+++ random.js
@@ -11,6 +11,20 @@
 
 //------------------------------------------------------------------------------
 
+// helping jenny w/ objects
+
+function compareObjects(obj1, obj2) {
+  for (let key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  for (let key in obj2) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+
+  return true;
+}
+
+
 
 //------------------------------------------------------------------------------
 
