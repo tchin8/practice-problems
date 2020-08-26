ac22ca5ada8c6a0252147e780dcc82592f701712
Tiffany <chin.tiffany108@gmail.com>
2020-08-26 13:38:40 -0400

    8/26

--- random.js
+++ random.js
@@ -16,6 +16,22 @@
 
 
 //------------------------------------------------------------------------------
+// IBM sample
+
+function fizzBuzz(n) {
+  // Write your code here
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 
 //------------------------------------------------------------------------------
