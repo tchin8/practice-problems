16261ae12af2e93e96fe608e05677a47b4a1a784
Tiffany <chin.tiffany108@gmail.com>
2020-09-01 12:48:15 -0400

    9/1

--- leetcode.js
+++ leetcode.js
@@ -37,6 +37,19 @@
 
 
 //------------------------------------------------------------------------------
+// 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+
+var getTargetCopy = function (original, cloned, target) {
+  if (!original) return null;
+
+  if (original === target) return cloned;
+
+  let left = getTargetCopy(original.left, cloned.left, target)
+  if (left) return left;
+
+  let right = getTargetCopy(original.right, cloned.right, target)
+  return right;
+};
 
 
 //------------------------------------------------------------------------------
