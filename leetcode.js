a3bf35db79fa24a7be5ce23e4bbf50b58ef3e8a9
Tiffany <chin.tiffany108@gmail.com>
2020-10-03 17:16:38 -0400

    saturday leetcode fun

--- leetcode.js
+++ leetcode.js
@@ -38,6 +38,26 @@ function TreeNode(val, left, right) {
 
 //------------------------------------------------------------------------------
 
+// 921. Minimum Add to Make Parentheses Valid
+var minAddToMakeValid = function (S) {
+  let left = 0;
+  let right = 0;
+
+  for (let i = 0; i < S.length; i++) {
+    if (S.charAt(i) == "(") {
+      left++;
+    } else {
+      if (left > 0) {
+        left--;
+      } else {
+        right++;
+      }
+    }
+  }
+
+  return left + right;
+};
+
 
 //------------------------------------------------------------------------------
 
